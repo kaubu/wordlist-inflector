9d6fdbb0e292f93e64d6e4a4a9bedd646063cbcc
unknown <kaution.burrowes@gmail.com>
2023-05-30 05:40:53 +1200

    Update

--- inflector.py
+++ inflector.py
@@ -15,7 +15,36 @@ from lemminflect import getAllInflections
 # }
 # type = <class 'dict'>
 
-with open(input("Word list: ./input/"), "r") as f:
-    pass
+words = []
+inflected_words = [] # Words + Inflections
 
-inflections = getAllInflections('watch')
+# Put all words in a list
+with open(
+        f"./input/{input('Word list: ./input/')}", "r", encoding="utf8"
+    ) as f:
+    for line in f:
+        words.append(line.strip())
+
+output_name = f"./output/{input('Output file name: ./output/')}"
+
+# Go through words, inflect them, and add to new list
+for word in words:
+    inflections = getAllInflections(word)
+    # Check if dictionary exists (i.e., not empty)
+    if not inflections:
+        continue
+    # print(f"debug inflections: {inflections}")
+
+    # Iterate in key, value pairs
+    for tag, new_words in inflections.items():
+        # new_words = tuple ('ACTED',)
+        
+        for new_word in new_words:
+            inflected_words.append(new_word)
+
+# Get rid of duplicates
+inflected_words = list(set(inflected_words))
+
+with open(output_name, "w", encoding="utf8") as f:
+    for word in inflected_words:
+        f.write(f"{word}\n")
